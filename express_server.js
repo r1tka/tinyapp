c2efbdfdfcb7f3ec96e5a7dc477d2c60172ea3ef
Margaryta <margaryta.lanova@gmail.com>
2022-10-03 20:26:13 -0700

    added a post route to receive the form submission and generated a random short URL ID

--- express_server.js
+++ express_server.js
@@ -9,9 +9,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let result = ""
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for ( let i = 0; i < 6; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+    characters.length));
+  }  
+  return result; 
+}
+console.log(generateRandomString())
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
