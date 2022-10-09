7af1e6621ed39637796154652ef9ebd11d40cce6
Margaryta <margaryta.lanova@gmail.com>
2022-10-09 16:27:02 -0700

    moved all functions to helpers. More comments

--- helpers.js
+++ helpers.js
@@ -1,11 +1,39 @@
+/* eslint-disable func-style */
+
+// Function used for getting user object from users by provided email
+// Return null if no user
 function getUserByEmail(email, users) {
   for (let userId in users) {
-    if(users[userId].email === email) {
-      return users[userId]
-    } 
+    if (users[userId].email === email) {
+      return users[userId];
+    }
   }
-     return null
-   }
+  return null;
+}
+
+// Function returning user urls by checking for incoming userId in urlDatabase.
+// Returns empty object if userId not matching userIds in urlDatabase
+const urlsForUser = function(userId, urlDatabase) {
+  let filterUrlDatabase = {};
+    
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].userID === userId) {
+      filterUrlDatabase = {...filterUrlDatabase, [key]: urlDatabase[key]};
+    }
+  }
+  return filterUrlDatabase;
+};
+
+// Function used for generating random 6 digits alfa-numeric string
+function generateRandomString() {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      characters.length));
+  }
+  return result;
+}
 
 
 
@@ -20,4 +48,4 @@ function getUserByEmail(email, users) {
 
 
 
-   module.exports = { getUserByEmail }
+module.exports = { getUserByEmail, urlsForUser, generateRandomString };
